ce50a50014552ccf022f3d3645feda1632a82b11
riyadrajan <riyadsrajan@gmail.com>
2026-01-05 01:48:13 -0400

    ML model created from Poisson Regression

--- Predicitor.py
+++ Predicitor.py
@@ -2,6 +2,11 @@ import psycopg2
 import pandas as pd
 import os
 from dotenv import load_dotenv
+from sklearn import linear_model
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import PoissonRegressor
 
 load_dotenv()
 
@@ -21,6 +26,7 @@ SELECT
     p.nationality,
     p.position,
     p.team,
+    s.season_id,
     s.season_year,
     ps.goals,
     ps.xG,
@@ -53,6 +59,79 @@ df["team_xg"] = df.groupby("team")["team_xg"].transform(
     lambda x: x.fillna(x.median())
 )
 
-bruno = df[df["player_id"] == 73]
-print(bruno)
+# bruno = df[df["player_id"] == 73]
+# print(bruno)
+# print(df.dtypes)
+
+# ohe = OneHotEncoder(
+#     handle_unknown="ignore",
+#     sparse_output=False 
+# )
+
+# encoded = ohe.fit_transform(df[["team", "position"]])
+
+# encoded_df = pd.DataFrame(
+#     encoded,
+#     columns=ohe.get_feature_names_out(["team", "position"])
+# )
+# bruno = encoded_df.iloc[363]
+# print(bruno)
+# rice = df[df["playername"] == "Declan Rice"]
+# print(rice)
+
+'''prepare dataframe for ML Poisson Regression
+Solve mixed data types in the df; we want the ML model to learn not base off of the stats alone, but also factors like team success'''
+
+num_features = ["xg", "g90", "team_xg"]
+cat_features = ["team", "position"] # to be one hot encoded
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ("num", "passthrough", num_features),
+        ("cat", OneHotEncoder(
+            handle_unknown="ignore",
+            sparse_output=False
+        ), cat_features)
+    ]
+)
+
+pipe = Pipeline(steps=[
+    ("preprocess", preprocessor),
+    ("model", PoissonRegressor(alpha=0.001, max_iter=1000))
+])
+
+#filter out test year (2024 - 2025)
+train_df = df[df["season_year"].isin([
+    "2020-2021",
+    "2021-2022",
+    "2022-2023",
+    "2023-2024"
+])]
+
+test_df = df[df["season_year"] == "2024-2025"]
+
+
+X_train = train_df[num_features + cat_features]
+y_train = train_df["goals"].astype(int)
+
+pipe.fit(X_train, y_train)
+test_df["predicted goals"] = pipe.predict(test_df[num_features + cat_features])
+
+#Analyse the encoded feature names
+feature_names = pipe.named_steps["preprocess"].get_feature_names_out()
+# print(feature_names)
+
+#Analyse the weighted coefficients produced by the model
+coefs = pipe.named_steps["model"].coef_
+
+coef_df = (
+    pd.DataFrame({
+        "feature": feature_names,
+        "coef": coefs
+    })
+    .sort_values("coef", ascending=False)
+)
+
+# print(coef_df)
+
 conn.close()
